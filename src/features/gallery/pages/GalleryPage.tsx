4ee53615c3e74b632255ede6d545b484b0987365
Karinacho <kari.cholakova@gmail.com>
2026-04-04 13:07:12 +0300

    feat: Grid Component

--- src/features/gallery/pages/GalleryPage.tsx
+++ src/features/gallery/pages/GalleryPage.tsx
@@ -1,12 +1,17 @@
-import {CardSkeleton} from "@/components/ui/Card";
+import { CardSkeleton} from "@/components/ui/Card";
+import Grid from "@/components/layout/Grid/Grid";
 
 const GalleryPage = () => {
   return (
     <div>
       <h1>Gallery</h1>
-        <div>
+        <Grid>
             <CardSkeleton />
-        </div>
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+            <CardSkeleton />
+        </Grid>
 
     </div>
   );
