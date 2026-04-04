f087e5a8a634c2fb7e780f2cccf8b4d94f94504e
Karinacho <kari.cholakova@gmail.com>
2026-04-04 12:39:19 +0300

    feat: Header component and RootLayout

--- src/app/providers/router.tsx
+++ src/app/providers/router.tsx
@@ -1,18 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "@/components/layout/RootLayout.tsx";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      lazy: async () => {
-        const GalleryPage = await import("@/features/gallery/pages/GalleryPage.tsx");
-        return { Component: GalleryPage.default };
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        lazy: async () => {
+          const GalleryPage = await import("@/features/gallery/pages/GalleryPage.tsx");
+          return { Component: GalleryPage.default };
+        },
       },
-    },
-    {
-      path: "/artifact/:id",
-      lazy: async () => {
-        const ArtifactPage = await import("@/features/artifact/pages/ArtifactPage.tsx");
-        return { Component: ArtifactPage.default };
+      {
+        path: "/artifact/:id",
+        lazy: async () => {
+          const ArtifactPage = await import("@/features/artifact/pages/ArtifactPage.tsx");
+          return { Component: ArtifactPage.default };
+        },
       },
-    },
-  ]);
+    ],
+  },
+]);
